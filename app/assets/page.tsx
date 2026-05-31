afbfd283ff57d085c49bfeea396411a6e2bc6d7a
mellaart <erik@mellaart.com>
2026-05-31 17:00:39 +0200

    Gebruik servicekosten binnen assets flow

--- app/assets/page.tsx
+++ app/assets/page.tsx
@@ -1,11 +1,5 @@
 import AssetsDashboard from "@/components/assets-dashboard";
-import ServiceCostsStep from "@/components/service-costs-step";
 
 export default function AssetsPage() {
-  return (
-    <>
-      <AssetsDashboard />
-      <ServiceCostsStep />
-    </>
-  );
+  return <AssetsDashboard />;
 }

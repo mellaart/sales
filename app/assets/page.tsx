ccce76d97b1236d420c8478a55ad86557ea69039
mellaart <erik@mellaart.com>
2026-06-02 07:26:35 +0200

    Keep assets page script-free

--- app/assets/page.tsx
+++ app/assets/page.tsx
@@ -1,12 +1,5 @@
-import Script from "next/script";
-
 import AssetsDashboard from "@/components/assets-dashboard";
 
 export default function AssetsPage() {
-  return (
-    <>
-      <AssetsDashboard />
-      <Script src="/customer-portal-current-sync.js" strategy="afterInteractive" />
-    </>
-  );
+  return <AssetsDashboard />;
 }

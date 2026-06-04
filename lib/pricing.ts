006e8ac5eba1ab878c758748daf6680abe42d592
mellaart <erik@mellaart.com>
2026-06-04 07:58:43 +0200

    Add Leverschema module pricing

--- lib/pricing.ts
+++ lib/pricing.ts
@@ -127,6 +127,7 @@ export const MODULES: ModuleConfig[] = [
   { key: "scanHerken", name: "Scan & Herken", monthlyPrice: 55 },
   { key: "statistiekenPlus", name: "Statistieken plus", monthlyPrice: 27.5 },
   { key: "digitaleOndertekening", name: "Digitale ondertekening", monthlyPrice: 27.5 },
+  { key: "leverschema", name: "Leverschema", monthlyPrice: 27.5 },
   { key: "postnl", name: "PostNL", monthlyPrice: 0 },
   { key: "suiteMkb", name: "Suite MKB koppeling", monthlyPrice: 55 },
   { key: "powerbi", name: "Power BI", monthlyPrice: 55 },

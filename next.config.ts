6d344726a1ddd3e09bf98727db7899de91787831
Erik Mellaart <erik@smarttrade.nl>
2026-06-16 10:58:15 +0200

    Bundle volledige Worldline OCR worker

--- next.config.ts
+++ next.config.ts
@@ -15,8 +15,14 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/api/worldline/documents/check": [
       "./node_modules/.pnpm/@tesseract.js-data+eng@*/node_modules/@tesseract.js-data/eng/**/*",
+      "./node_modules/.pnpm/bmp-js@*/node_modules/bmp-js/**/*",
+      "./node_modules/.pnpm/is-url@*/node_modules/is-url/**/*",
+      "./node_modules/.pnpm/node-fetch@*/node_modules/node-fetch/**/*",
+      "./node_modules/.pnpm/regenerator-runtime@*/node_modules/regenerator-runtime/**/*",
       "./node_modules/.pnpm/tesseract.js-core@*/node_modules/tesseract.js-core/**/*",
-      "./node_modules/.pnpm/tesseract.js@*/node_modules/tesseract.js/src/worker-script/node/**/*",
+      "./node_modules/.pnpm/tesseract.js@*/node_modules/tesseract.js/**/*",
+      "./node_modules/.pnpm/wasm-feature-detect@*/node_modules/wasm-feature-detect/**/*",
+      "./node_modules/.pnpm/zlibjs@*/node_modules/zlibjs/**/*",
     ],
   },
   async headers() {
